bf7901a683af373cae2afb03f14680d3a7bfa293
summer3484 <summergarry94@gmail.com>
2026-05-09 12:45:49 -0400

    Add files via upload

---
+++ 1cw.py
@@ -0,0 +1,31 @@
+## Create a turtle move a turtle turn the turtle left and right.
+##import turtle # this allows us to use turtles
+##BillGates = turtle.Turtle() # read this like 'BillGates is a turtle'
+##
+##BillGates.forward(100) # the number 100 here means 100 steps
+##BillGates.backward(50)
+##
+####turtle.done() # use this as the last line!
+##
+##BillGates.left(90) # the number is how many degrees to turn
+##BillGates.right(180) # 180 will face you the oppisite direction
+
+#CW
+import turtle
+abc = turtle.Turtle() #read this like 'abc is a turtle'
+abc.forward(100) #the number 100 here means 100 steps
+abc.right(90) #the number 90 means 90 degrees
+abc.forward(100) #the number 100 here means 100 steps
+abc.right(90) #the number 90 means 90 degrees
+abc.forward(100) #the number 100 here means 100 steps
+abc.right(90) #the number 90 here means 90 degrees
+abc.forward(100) #the number 100 here means 100 steps
+##turtle.done() #use this as the last
+abc.backward(100) # the number 100 here means 100 steps
+abc.left(90) #the number 90 here means 90 degrees
+abc.backward(100) # the number 100 here means 100 steps
+abc.left(90) #the number 90 here means 90 degrees
+abc.backward(100) # the number 100 here means 100 steps
+abc.left(90) #the number 90 here means 90 degrees
+abc.backward(100)
+
